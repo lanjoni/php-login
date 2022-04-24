a5f335da55cd7ad6d8487c1e943a2ff354f40247
guto <91899995+gutoso@users.noreply.github.com>
2022-04-24 00:26:57 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 
 define('HOST', '127.0.0.1');
 define('USER', 'root');
-define('PASSWORD', 'pgserver');
+define('PASSWORD', 'your password!');
 define('DB', 'IFSP');
 
 $conexao = mysqli_connect(HOST, USER, PASSWORD, DB) or die ('Não foi possível conectar!');
